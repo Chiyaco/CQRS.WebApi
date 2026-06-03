fc2560713ef0f8ea7ccc34527fb00b734c791ea2
Chia.Karimi <chiya.sne@gmail.com>
2026-06-04 01:18:28 +0200

    Add Entities to the domain layer

---
+++ SaaSPlatform.Domain/Entities/Customer/Email.cs
@@ -0,0 +1,26 @@
+﻿namespace SaaSPlatform.Domain.Entities.Customer;
+
+public sealed class Email
+{
+    public string Value { get; }
+
+    private Email() { }
+
+    public Email(string email)
+    {
+        if (string.IsNullOrWhiteSpace(email))
+            throw new ArgumentNullException("Email address is required");
+
+        if (!IsValid(email))
+            throw new ArgumentException("The email address is not valid.");
+
+        Value = email;
+    }
+
+    protected static bool IsValid(string email)
+    {
+        return new System.ComponentModel.DataAnnotations.EmailAddressAttribute().IsValid(email);
+    }
+
+    public override string ToString() => Value;
+}
