ea60f2ba4aa89c274d52ae84458413956922aa94
Chia.Karimi <chiya.sne@gmail.com>
2026-06-06 22:55:55 +0200

    Seperate the applicationDbContext concept to the Application layer

---
+++ SaaSPlatform.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SaaSPlatform.Domain.Entities.Customer;
+using SaaSPlatform.Domain.Entities.Order;
+using SaaSPlatform.Domain.Entities.Product;
+
+namespace SaaSPlatform.Application.Common.Interfaces;
+public interface IApplicationDbContext
+{
+    DbSet<Customer> Customers { get;}
+    DbSet<Order> Orders { get;}
+    DbSet<Product> Products { get;}
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
